e4fc5bd38c9e88bc370ac290b4065a2798f20cd4
ekankr2 <ekankr2@naver.com>
2024-10-01 21:06:19 +0900

    feat: discord bot test

---
+++ app/discord/test/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest } from "next/server";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v10";
+
+/**
+ * @swagger
+ * /discord/test:
+ *   get:
+ *     summary: 디스코드 테스트
+ *     tags: [test]
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+export async function GET(request: NextRequest) {
+  const rest = new REST({ version: "10" }).setToken(
+    process.env.DISCORD_TOKEN || "",
+  );
+
+  try {
+    await rest.post(Routes.channelMessages("1290352667302035488"), {
+      body: {
+        content: "A message via REST!",
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
