1498d8ddae30720981caff57d0bb0688875934de
IkHwan Im <ekankr2@naver.com>
2024-10-02 10:22:55 +0900

    remove discord test

--- app/discord/test/route.ts
+++ app/discord/test/route.ts
@@ -13,20 +13,20 @@ import { Routes } from "discord-api-types/v10";
  *         description: OK
  */
 export async function GET(request: NextRequest) {
-  const rest = new REST({ version: "10" }).setToken(
-    process.env.DISCORD_TOKEN || "",
-  );
-
-  try {
-    await rest.post(Routes.channelMessages("1290352667302035488"), {
-      body: {
-        content: "A message via REST!\n 줄바꿈",
-      },
-    });
-  } catch (error) {
-    console.error(error);
-  }
-  console.log('discord test');
+  // const rest = new REST({ version: "10" }).setToken(
+  //   process.env.DISCORD_TOKEN || "",
+  // );
+  //
+  // try {
+  //   await rest.post(Routes.channelMessages("1290352667302035488"), {
+  //     body: {
+  //       content: "A message via REST!\n 줄바꿈",
+  //     },
+  //   });
+  // } catch (error) {
+  //   console.error(error);
+  // }
+  // console.log('discord test');
 
   return NextResponse.json({ success: true }, { status: 200 });
 }
