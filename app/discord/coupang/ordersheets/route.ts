b9d54da2d6556a54d479d1caa2db7121fd2a7581
IkHwan Im <ekankr2@naver.com>
2024-10-02 10:10:00 +0900

    fix: route wrong

--- app/discord/coupang/ordersheets/route.ts
+++ app/discord/coupang/ordersheets/route.ts
@@ -4,6 +4,16 @@ import dayjs from "dayjs";
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v10";
 
+/**
+ * @swagger
+ * /discord/coupang/ordersheets:
+ *   get:
+ *     summary: 디스코드 테스트
+ *     tags: [디스코드]
+ *     responses:
+ *       200:
+ *         description: OK
+ */
 export async function GET(request: NextRequest) {
   const rest = new REST({ version: "10" }).setToken(
     process.env.DISCORD_TOKEN || "",
@@ -21,12 +31,12 @@ export async function GET(request: NextRequest) {
       coupangService.OrderStatus.결제완료,
     );
     const ordersheets = await response.json();
-    if (ordersheets.data) {
-      await rest.post(Routes.channelMessages("1290352667302035488"), {
-        body: {
-          content: `쿠팡 새로운 주문 *${ordersheets.data.length}* 건 있음`,
-        },
-      });
+    if (ordersheets.data && ordersheets.data.length > 0) {
+      // await rest.post(Routes.channelMessages("1290352667302035488"), {
+      //   body: {
+      //     content: `쿠팡 새로운 주문 *${ordersheets.data.length}* 건 있음`,
+      //   },
+      // });
     }
     return NextResponse.json({ success: true }, { status: 200 });
   } catch (error) {
