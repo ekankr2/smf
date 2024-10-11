c22357b45cf1636cd0d7776d5686769cad9439c7
ekankr2 <ekankr2@naver.com>
2024-10-11 19:35:32 +0900

    remove boilerplate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +12,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="kr">
+      <body>
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          {children}
+        </main>
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
       </body>
     </html>
   );
